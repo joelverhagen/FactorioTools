05d3100f069a678ac9f529ef6e8d7425f5a63e44
Joel Verhagen <joel.verhagen@gmail.com>
2023-02-08 13:18:50 -0500

    Perf: explore paths simply instead of using A*

--- src/FactorioTools/OilField/Steps/X.RotateOptimize.cs
+++ src/FactorioTools/OilField/Steps/X.RotateOptimize.cs
@@ -9,11 +9,12 @@ internal static class RotateOptimize
     internal static HashSet<Location> Execute(Context context, HashSet<Location> pipes)
     {
         var intersections = GetIntersections(context, pipes);
+        var goals = GetGoals(context, intersections);
 
         var existingPipeGrid = new ExistingPipeGrid(context.Grid);
         AddPipeEntities.Execute(existingPipeGrid, context.CenterToTerminals, pipes);
 
-        // Visualizer.Show(existingPipeGrid, intersections.Select(l => (IPoint)new Point(l.X, l.Y)), Array.Empty<IEdge>());
+        Visualizer.Show(existingPipeGrid, intersections.Select(l => (DelaunatorSharp.IPoint)new DelaunatorSharp.Point(l.X, l.Y)), Array.Empty<DelaunatorSharp.IEdge>());
 
         foreach ((var center, var terminals) in context.CenterToTerminals)
         {
@@ -29,23 +30,17 @@ internal static class RotateOptimize
                 continue;
             }
 
-            var newPipes = new HashSet<Location>(pipes);
-
-            var goals = new HashSet<Location>(intersections);
-            goals.UnionWith(context.LocationToTerminals.Keys);
-
-            var paths = new List<(TerminalLocation Terminal, List<Location> Path)>();
-
             goals.Remove(currentTerminal.Terminal);
+            var exploredPaths = ExplorePaths(existingPipeGrid, currentTerminal.Terminal, goals);
 
-            var originalPathResult = AStar.GetShortestPath(existingPipeGrid, currentTerminal.Terminal, goals);
-            var originalPathGoal = originalPathResult.ReachedGoal!.Value;
-            var originalPath = originalPathResult.Path;
+            var goal = exploredPaths.ReachedGoals.Single();
+            var newPipes = new HashSet<Location>(pipes);
+            exploredPaths.RemovePathTo(goal, newPipes);
 
-            newPipes.ExceptWith(originalPath);
-            newPipes.Add(originalPathGoal);
-
-            paths.Add((currentTerminal, originalPath));
+            var paths = new List<(TerminalLocation Terminal, int PathLength, List<Location>? Path)>
+            {
+                (currentTerminal, PathLength: pipes.Count - newPipes.Count + 1, Path: null)
+            };
 
             foreach ((var direction, var translation) in InitializeContext.TerminalOffsets)
             {
@@ -61,15 +56,15 @@ internal static class RotateOptimize
                 if (result.ReachedGoal.HasValue)
                 {
                     var path = result.Path;
-                    paths.Add((new TerminalLocation(center, terminalCandidate, direction), path));
+                    paths.Add((new TerminalLocation(center, terminalCandidate, direction), path.Count, path));
                 }
             }
 
-            paths = paths.OrderBy(p => p.Path.Count).ToList();
+            paths = paths.OrderBy(p => p.PathLength).ToList();
 
-            if (paths[0].Path.Count < originalPath.Count)
+            if (paths[0].Path is not null)
             {
-                var newPath = paths[0].Path;
+                var newPath = paths[0].Path!;
                 var newTerminal = paths[0].Terminal;
 
                 newPipes.UnionWith(newPath);
@@ -94,6 +89,7 @@ internal static class RotateOptimize
                 pipes = newPipes;
 
                 intersections = GetIntersections(context, pipes);
+                goals = GetGoals(context, intersections);
 
                 /*
                 var clone = new PipeGrid(context.Grid);
@@ -101,9 +97,59 @@ internal static class RotateOptimize
                 Visualizer.Show(clone, originalPath.Select(l => (IPoint)new Point(l.X, l.Y)), Array.Empty<IEdge>());
                 */
             }
+            else
+            {
+                // This goal was removed above so we add it back for subsequent iterations.
+                goals.Add(currentTerminal.Terminal);
+            }
         }
 
         return pipes;
+    }
+
+    private static HashSet<Location> GetGoals(Context context, HashSet<Location> intersections)
+    {
+        var goals = new HashSet<Location>(intersections);
+        goals.UnionWith(context.LocationToTerminals.Keys);
+        return goals;
+    }
+
+    private static ExploredPaths ExplorePaths(SquareGrid grid, Location start, HashSet<Location> goals)
+    {
+        var cameFrom = new Dictionary<Location, Location>();
+        cameFrom[start] = start;
+
+        var toExplore = new Stack<Location>();
+        toExplore.Push(start);
+
+        Span<Location> neighbors = stackalloc Location[4];
+
+        var reachedGoals = new List<Location>();
+
+        while (toExplore.Count > 0)
+        {
+            var current = toExplore.Pop();
+
+            if (current != start && goals.Contains(current))
+            {
+                reachedGoals.Add(current);
+                continue;
+            }
+
+            grid.GetNeighbors(neighbors, current);
+            for (var i = 0; i < neighbors.Length; i++)
+            {
+                if (!neighbors[i].IsValid || cameFrom.ContainsKey(neighbors[i]))
+                {
+                    continue;
+                }
+
+                cameFrom.Add(neighbors[i], current);
+                toExplore.Push(neighbors[i]);
+            }
+        }
+
+        return new ExploredPaths(cameFrom, reachedGoals);
     }
 
     private static HashSet<Location> GetIntersections(Context context, HashSet<Location> pipes)
@@ -140,5 +186,38 @@ internal static class RotateOptimize
         }
 
         return intersections;
+    }
+
+    private class ExploredPaths
+    {
+        public ExploredPaths(Dictionary<Location, Location> cameFrom, List<Location> reachedGoals)
+        {
+            CameFrom = cameFrom;
+            ReachedGoals = reachedGoals;
+        }
+
+        public Dictionary<Location, Location> CameFrom { get; }
+        public List<Location> ReachedGoals { get; }
+
+        public void RemovePathTo(Location goal, HashSet<Location> removeFrom)
+        {
+            var current = goal;
+
+            while (true)
+            {
+                if (!CameFrom.TryGetValue(current, out var previous))
+                {
+                    throw new InvalidOperationException("No path to this goal was found.");
+                }
+
+                if (current == previous)
+                {
+                    break;
+                }
+
+                removeFrom.Remove(previous);
+                current = previous;
+            }
+        }
     }
 }
