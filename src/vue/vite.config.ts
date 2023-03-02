d830306ba6171e8c0eb87bea34df3d2c0a43c95c
Joel Verhagen <joel.verhagen@gmail.com>
2023-03-01 23:25:50 -0500

    Working on deploy

--- src/vue/vite.config.ts
+++ src/vue/vite.config.ts
@@ -7,7 +7,6 @@ const buildDate = new Date().toISOString().slice(0, 10);
 const commit = execSync('git rev-parse --short HEAD').toString().trim()
 const shortVersion = execSync('git describe --abbrev=0').toString()
 let version = execSync('git describe --long --dirty').toString()
-console.log(buildDate, commit, shortVersion, version)
 if (version == `${shortVersion}-0-g${commit}`) {
   version = shortVersion
 }
