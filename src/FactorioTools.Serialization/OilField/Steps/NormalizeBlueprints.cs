275992af6389f54a62ecfbb3a6e627cc8f98b494
Joel Verhagen <joel.verhagen@gmail.com>
2023-12-28 12:36:50 -0500

    Change normalization

--- src/FactorioTools.Serialization/OilField/Steps/NormalizeBlueprints.cs
+++ src/FactorioTools.Serialization/OilField/Steps/NormalizeBlueprints.cs
@@ -9,36 +9,61 @@ public static class NormalizeBlueprints
 {
     public static void Execute(string inputPath, string existingPath)
     {
-        var existing = NormalizeFile(existingPath).Select(x => x.Normalized).ToHashSet();
+        var existing = NormalizeFile(existingPath).ToLookup(x => x.Normalized);
 
-        var input = NormalizeFile(inputPath).Where(b => !existing.Contains(b.Normalized)).ToArray();
+        var input = NormalizeFile(inputPath);
 
-        File.WriteAllLines(inputPath, input.Select(b => b.Normalized).Distinct().Order().ToArray());
+        var output = new List<string>();
+        var added = new HashSet<string>();
+        var orderedInput = input
+            .Concat(existing.SelectMany(g => g))
+            .Where(x => x.Valid)
+            .OrderBy(x => existing[x.Normalized].Select(x => x.Index).DefaultIfEmpty(int.MaxValue).Min())
+            .ThenBy(x => x.Index);
+
+        foreach (var line in orderedInput)
+        {
+            if (line.Normalized is null)
+            {
+                continue;
+            }
+
+            if (added.Add(line.Normalized))
+            {
+                output.Add(line.Normalized);
+            }
+        }
+
+
+        File.WriteAllLines(inputPath, output);
     }
 
-    private static List<(string Original, string Normalized)> NormalizeFile(string inputPath)
+    private static List<(string Original, string? Normalized, int Index, bool Valid)> NormalizeFile(string inputPath)
     {
-        var lines = new List<(string Original, string Normalized)>();
-        foreach (var blueprintString in File.ReadAllLines(inputPath))
+        var lines = new List<(string Original, string? Normalized, int Index, bool Valid)>();
+        var index = 0;
+        foreach (var blueprintString in File.ReadLines(inputPath))
         {
             try
             {
-                string output = Normalize(blueprintString, includeFbeOffset: false);
-                lines.Add((blueprintString, output));
+                var valid = TryNormalize(blueprintString, includeFbeOffset: false, out var output);
+                lines.Add((blueprintString, output, index, valid));
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
             }
+
+            index++;
         }
 
         return lines;
     }
 
-    public static string Normalize(string blueprintString, bool includeFbeOffset)
+    private static bool TryNormalize(string blueprintString, bool includeFbeOffset, out string? normalized)
     {
         var trimmed = blueprintString.Trim();
-        var output = blueprintString;
+
         if (trimmed.Length > 0 && !trimmed.StartsWith("#"))
         {
             var blueprint = ParseBlueprint.Execute(trimmed);
@@ -48,9 +73,11 @@ public static class NormalizeBlueprints
                 throw new InvalidOperationException("There are not pumpjacks in the blueprint.");
             }
 
-            output = GridToBlueprintString.SerializeBlueprint(clean, includeFbeOffset);
+            normalized = GridToBlueprintString.SerializeBlueprint(clean, includeFbeOffset);
+            return true;
         }
 
-        return output;
+        normalized = null;
+        return false;
     }
 }
