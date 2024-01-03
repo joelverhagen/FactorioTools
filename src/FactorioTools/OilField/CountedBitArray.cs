c5d00e328dbb5fbab340687ef54025b08cbad4ba
Joel Verhagen <jver@microsoft.com>
2024-01-03 14:59:49 -0500

    Allow BitArray to be disabled

--- src/FactorioTools/OilField/CountedBitArray.cs
+++ src/FactorioTools/OilField/CountedBitArray.cs
@@ -1,23 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 
 namespace Knapcode.FactorioTools.OilField;
 
 public class CountedBitArray
 {
+#if USE_BITARRAY
     private readonly BitArray _array;
+#else
+    private readonly bool[] _array;
+#endif
 
     public CountedBitArray(CountedBitArray bits)
     {
+#if USE_BITARRAY
         _array = new BitArray(bits._array);
+#else
+        _array = new bool[bits.Count];
+        Array.Copy(bits._array, _array, bits.Count);
+#endif
         TrueCount = bits.TrueCount;
     }
 
     public CountedBitArray(int length)
     {
+#if USE_BITARRAY
         _array = new BitArray(length);
+#else
+        _array = new bool[length];
+#endif
     }
 
+#if USE_BITARRAY
     public int Count => _array.Count;
+#else
+    public int Count => _array.Length;
+#endif
 
     public bool this[int index]
     {
@@ -38,37 +56,72 @@ public class CountedBitArray
 
     public bool All(bool value)
     {
-        return value ? TrueCount == _array.Count : TrueCount == 0;
+        return value ? TrueCount == Count : TrueCount == 0;
     }
 
     public bool Any(bool value)
     {
-        return value ? TrueCount > 0 : TrueCount < _array.Count;
+        return value ? TrueCount > 0 : TrueCount < Count;
     }
 
     public void SetAll(bool value)
     {
+#if USE_BITARRAY
         _array.SetAll(value);
-        TrueCount = value ? _array.Count : 0;
+#else
+        Array.Fill(_array, value);
+#endif
+        TrueCount = value ? Count : 0;
     }
 
     public CountedBitArray And(CountedBitArray value)
     {
+#if USE_BITARRAY
         _array.And(value._array);
+#else
+        if (value.Count != Count)
+        {
+            throw new ArgumentException("The two bit arrays must have the same number of elements.");
+        }
+
+        for (var i = 0; i < Count; i++)
+        {
+            _array[i] = _array[i] && value[i];
+        }
+#endif
         TrueCount = CountTrue();
         return this;
     }
 
     public CountedBitArray Not()
     {
+#if USE_BITARRAY
         _array.Not();
+#else
+        for (var i = 0; i < Count; i++)
+        {
+            _array[i] = !_array[i];
+        }
+#endif
         TrueCount = Count - TrueCount;
         return this;
     }
 
     public CountedBitArray Or(CountedBitArray value)
     {
+#if USE_BITARRAY
         _array.Or(value._array);
+#else
+        if (value.Count != Count)
+        {
+            throw new ArgumentException("The two bit arrays must have the same number of elements.");
+        }
+
+        for (var i = 0; i < Count; i++)
+        {
+            _array[i] = _array[i] || value[i];
+        }
+#endif
         TrueCount = CountTrue();
         return this;
     }
