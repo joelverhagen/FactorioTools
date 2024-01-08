7f86dde199372225892db516021100723b252752
Joel Verhagen <joel.verhagen@gmail.com>
2024-01-08 12:47:40 -0500

    Avoid using aggressive inline in indexer
    Workaround for https://github.com/yanghuan/CSharp.lua/issues/485

--- src/FactorioTools/OilField/Containers/LocationIntDictionary.cs
+++ src/FactorioTools/OilField/Containers/LocationIntDictionary.cs
@@ -23,8 +23,9 @@ public class LocationIntDictionary<T> : ILocationDictionary<T>
 
     public T this[Location index]
     {
-        get => _dictionary[GetIndex(index)];
-        set => _dictionary[GetIndex(index)] = value;
+        // should use GetIndex, but blocked by https://github.com/yanghuan/CSharp.lua/issues/485
+        get => _dictionary[index.Y * _width + index.X];
+        set => _dictionary[index.Y * _width + index.X] = value;
     }
 
     public int Count => _dictionary.Count;
